a0b84ddf9d1c39f3e7335601d301c7a4000d00a7
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2023-10-29 22:17:47 +0100

    first Release

--- main.ts
+++ main.ts
@@ -1,1 +1,23 @@
+namespace modules {
+    /**
+     * The Soilmoisture measured by Grove moisture Sensor at C16
+     */
+    //% fixedInstance whenUsed block="Grove SoilMoistureLevel"
+    export const GroveSoilMoistureLevel = new SoilMoistureClient("C16?dev=self")
+}
 
+namespace servers {
+    function start() {
+        jacdac.productIdentifier = 0x30912064
+        jacdac.deviceDescription = "Grove soil moisture"
+        jacdac.startSelfServers(() => [
+            jacdac.createSimpleSensorServer(
+                jacdac.SRV_SOIL_MOISTURE,
+                jacdac.SoilMoistureRegPack.Moisture,
+                () => pins.analogReadPin(AnalogPin.C16) / 1023,
+                { streamingInterval: 100 }
+            ),
+        ])
+    }
+    start()
+}
