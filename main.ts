2c1a5c12e257add2ee11977ee14a0a3d17addd7b
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2024-05-01 21:10:03 +0200

    Deutsche Übersetzung

--- main.ts
+++ main.ts
@@ -2,14 +2,14 @@ namespace modules {
     /**
      * The Soilmoisture measured by Grove moisture Sensor at C16
      */
-    //% fixedInstance whenUsed block="Grove SoilMoistureLevel"
-    export const GroveSoilMoistureLevel = new SoilMoistureClient("C16?dev=self")
+    //% fixedInstance whenUsed block="Grove soil moisture"
+    //% block.loc.de="Grove Bodenfeuchtesensor"
+      export const GroveSoilMoistureLevel = new SoilMoistureClient("C16?dev=self")
 }
-
 namespace servers {
     function start() {
         jacdac.productIdentifier = 0x30912064
-        jacdac.deviceDescription = "Grove soil moisture"
+        jacdac.deviceDescription = "Grove soil moisture" 
         jacdac.startSelfServers(() => [
             jacdac.createSimpleSensorServer(
                 jacdac.SRV_SOIL_MOISTURE,
